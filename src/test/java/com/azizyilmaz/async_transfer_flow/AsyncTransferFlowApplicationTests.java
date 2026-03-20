153fecb56fccf4f1edea5d00508766119e77c398
azizyilmaz <azizxyilmaz@outlook.com>
2026-03-20 22:57:49 +0300

    Initial commit

---
+++ src/test/java/com/azizyilmaz/async_transfer_flow/AsyncTransferFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azizyilmaz.async_transfer_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncTransferFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
